e5a8431761aadaf35b0d8501e8fb34eff98fde8b
Sergio Marques <sergimarques@hotmail.com>
2018-12-11 12:13:53 +0100

    PUNTO2

--- src/practica/Reloj.java
+++ src/practica/Reloj.java
@@ -1,5 +1,12 @@
 package practica;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class Reloj {
 
 	private int hora, minutos, segundos;
@@ -32,4 +39,26 @@ public class Reloj {
 		this.segundos = segundos;
 	}
 	
+	public void write() {
+		File origen = new File("origen.txt");
+		File destino = new File("destino.txt");
+
+		try {
+		  InputStream in = new FileInputStream(origen);
+		  OutputStream out = new FileOutputStream(destino);
+						
+		  byte[] buf = new byte[1024];
+		  int len;
+
+		  while ((len = in.read(buf)) > 0) {
+		    out.write(buf, 0, len);
+		  }
+			
+		  in.close();
+		  out.close();
+		} catch (IOException ioe){
+		  ioe.printStackTrace();
+		}
+
+	}
 }
