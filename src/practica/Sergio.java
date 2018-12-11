8aee979aefcd33436ef7684e0d3f8306d3a2f357
Sergio Marques <sergimarques@hotmail.com>
2018-12-11 12:01:52 +0100

    PUNTO1

---
+++ src/practica/Sergio.java
@@ -0,0 +1,35 @@
+package practica;
+
+public class Sergio {
+
+	private String nombre, apellido, dni;
+	
+	public Sergio() {
+		
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	
+}
