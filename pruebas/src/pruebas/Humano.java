76917f1d5f1b6e4c94646cafe24577d81f5da88e
Sergio Marques <sergimarques@hotmail.com>
2018-12-11 11:47:41 +0100

    PUNTO1

--- pruebas/src/pruebas/Humano.java
+++ pruebas/src/pruebas/Humano.java
@@ -1,5 +1,21 @@
 package pruebas;
 
+import java.io.File;
+
 public class Humano {
 
+	
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	private void initTodo() {
+		File fichero = new File("hola.txt");
+	}
 }
